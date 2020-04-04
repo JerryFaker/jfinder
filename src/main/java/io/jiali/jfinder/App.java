4134f32190ed2172258aa642efbc7b35fdd6e1a0
JerryJia <tinyfantasy@outlook.com>
2020-04-04 16:16:25 +0800

    implement all function except encrypt/decrypt

--- src/main/java/io/jiali/jfinder/App.java
+++ src/main/java/io/jiali/jfinder/App.java
@@ -1,67 +1,71 @@
 package io.jiali.jfinder;
+
 import java.io.File;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "你好，欢迎使用JFinder" );
-        System.out.println("温馨提示：随时输入help，获取使用帮助");
-        System.out.println("祝您玩得开心，过得愉快");
-        System.out.println("输入quit，结束程序");
+public class App {
 
-        JFinder jFinder = new JFinder();
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    JFinder jFinder = new JFinder();
+    Scanner scanner = new Scanner(System.in);
 
-        boolean isRun = true;
-        while(isRun)
-        {
-        String input = scanner.nextLine();
+    jFinder.hello();
 
-        switch (input) {
-            case "help": {
-                jFinder.help();
-                break;
-            }
+    boolean isRun = true;
 
-            case "quit": {
-                isRun = false;
-                break;
-            }
+    while (isRun) {
+      String[] input = jFinder.input(scanner.nextLine());
 
-            case "cd": {
-                System.out.println("输入路径名：");
-                File f = new File(scanner.nextLine());
-                jFinder.cd(f);
-                break;
-            }
-
-            case "ls": {
-                jFinder.list();
-                break;
-            }
-
-            case "mkdir": {
-                System.out.println("输入新建文件夹名称，默认为\"新建文件夹\"：");
-                String s = scanner.nextLine();
-                jFinder.makeDir(new File(jFinder.currentPath.getName()+"/"+(s.isEmpty()? "新建文件夹" : s)));
-                /*if (s.isEmpty())
-                {
-                    jFinder.makeDir(new File(jFinder.currentPath.getName()+"/新建文件夹"));
-                }
-                else
-                {
-                    jFinder.makeDir(new File(jFinder.currentPath.getName()+"/"+s));
-                }
-                */
-            }
+      switch (input[0]) {
+        case "help": {
+          jFinder.help();
+          break;
         }
-    }
-    }
 
+        case "quit": {
+          isRun = false;
+          break;
+        }
+
+        case "cd": {
+          jFinder.cd(new File(input[1].isEmpty() ? jFinder.getInput("目录名") : input[1]));
+          break;
+        }
+
+        case "ls": {
+          jFinder.list();
+          break;
+        }
+
+        case "mkdir": {
+          String name = input[1];
+          if (input[1].isEmpty()) {
+            name = jFinder.getInput("新建目录的名称");
+          }
+          jFinder.makeDir(new File(jFinder.getCurrentPath().getAbsolutePath(), name));
+          break;
+        }
+
+        case "rmdir": {
+          String name = input[1];
+          if (input[1].isEmpty()) {
+            name = jFinder.getInput("要删除的目录");
+          }
+          jFinder.removeDir(new File(name));
+          break;
+        }
+
+        case "cp": {
+          jFinder.copy(jFinder.copyCheck(input[1]), new File(jFinder.getInput("目标目录名")));
+          break;
+        }
+
+        default: {
+          System.out.println("无法识别指令!请重新输入");
+          break;
+        }
+      }
+    }
+    scanner.close();
+  }
 }
--- src/main/java/io/jiali/jfinder/JFinder.java
+++ src/main/java/io/jiali/jfinder/JFinder.java
@@ -4,131 +4,160 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class JFinder {
+class JFinder {
 
-    public static File currentPath;
+  private File currentPath = new File("");
 
-    void cd(File f) {
-        if (f.exists() && f.isDirectory())
-        {
-            currentPath = f;
-            System.out.println("目前路径为："+f.getName());
+  File getCurrentPath() {
+    return currentPath;
+  }
+
+  String getInput(String s) {
+    System.out.println("请输入" + s);
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextLine();
+  }
+
+  String[] input(String input) {
+    String[] output = new String[2];
+    if (input.contains(" ")) {
+      output = input.split(" ", 2);
+    } else {
+      output[0] = input;
+      output[1] = "";
+    }
+    return output;
+  }
+
+  void cd(File f) {
+    if (f.exists() && f.isDirectory()) {
+      currentPath = f;
+      System.out.println("当前路径为：" + f.getAbsolutePath());
+    } else {
+      System.out.println("目录不存在！");
+    }
+  }
+
+  void list() {
+    File f = currentPath;
+    if (f.exists() && f.isDirectory()) {
+      File[] files = f.listFiles();
+      int count = 0;
+      assert files != null;
+      for (File file : files) {
+        System.out.print(file.getName());
+        count++;
+        if (count < 4) {
+          System.out.print("    ");
+        } else {
+          System.out.println();
+          count = 0;
         }
-        else
-        {
-            System.out.println("目录不存在！");
+      }
+      System.out.println("");
+    } else {
+      System.out.println("目录或文件不存在！");
+    }
+  }
+
+  void makeDir(File f) {
+    if (!f.exists()) {
+      f.mkdir();
+      System.out.println("名为" + f.getName() + "的新目录成功创建");
+
+    } else {
+      System.out.println("已存在同名目录!");
+    }
+  }
+
+  void removeDir(File f) {
+    File[] files = f.listFiles();
+    if (files != null) {
+      for (File file : files) {
+        if (file.isDirectory()) {
+          removeDir(file);
+        } else {
+          file.delete();
+          System.out.println("成功删除文件 "+file.getName());
         }
+      }
+    }
+    if (f.exists()) {
+      f.delete();
+      System.out.println("成功删除目录 "+f.getName());
+    } else {
+      System.out.println("操作失败，" + f.getName() + "不存在");
+    }
+  }
+
+  void copy(File src, File dest) {
+    if (!dest.exists()) {
+      dest.mkdirs();
+    }
+    if (src.isDirectory()) {
+      String[] fs = src.list();
+      dest = new File(dest.getAbsolutePath(), src.getName());
+      dest.mkdirs();
+      assert fs != null;
+      for (String f : fs) {
+        copy(new File(src, f), dest);
+      }
+    } else {
+      dest = new File(dest.getAbsolutePath(), src.getName());
+      try (FileInputStream inStream = new FileInputStream(src);
+          FileOutputStream outStream = new FileOutputStream(dest);) {
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inStream.read(buffer)) > 0) {
+          outStream.write(buffer, 0, length);
+        }
+        System.out.println("文件复制成功! "+dest.getAbsolutePath()+File.separator+dest.getName());
+        inStream.close();
+        outStream.close();
+      } catch (IOException e) {
+        System.out.println("出现错误，请检查文件："+dest.getAbsolutePath()+File.separator+dest.getName());
+      }
     }
 
-    void list() {
-        File f = currentPath;
-        if (f.exists() && f.isDirectory())
-        {
-            File[] files = f.listFiles();
-            int count = 0;
-            for (File file : files) {
-                System.out.print(file.getName());
-                count++;
-                if (count < 4) {
-                    System.out.print("    ");
-                }
-                else
-                {
-                    System.out.println();
-                    count = 0;
-                }
-            }
+  }
+
+  File copyCheck(String s) {
+    File src = new File("");
+    if (!s.isEmpty()) {
+      if (new File(s).exists()) {
+        src = new File(s);
+      } else {
+        File f = new File(getCurrentPath().getAbsolutePath(), s);
+        if (f.exists()) {
+          src = f;
+        } else {
+          System.out.println("目录或文件不存在！");
+          src = copyCheck(getInput("要复制的文件或目录名"));
         }
-        else
-        {
-            System.out.println("目录或文件不存在！");
-        }
+      }
+    } else {
+      src = copyCheck(getInput("要复制的文件或目录名"));
     }
+    return src;
+  }
 
-    void makeDir(File f) {
-        if (!f.exists())
-        {
-            f.mkdir();
-            System.out.println("名为"+f.getName()+"的新目录成功创建");
+  void hello() {
+    System.out.println("你好，欢迎使用JFinder");
+    System.out.println("温馨提示：随时输入help，获取使用帮助");
+    System.out.println("祝您玩得开心，过得愉快");
+    System.out.println("输入quit，结束程序");
+  }
 
-        }
-        else
-        {
-            System.out.println("已存在同名目录!");
-        }
-    }
-
-    void copy(File src, File dest) {
-        if (src.isDirectory())
-        {
-            if (!dest.exists())
-            {
-                dest.mkdir();
-
-                String fs[] = src.list();
-                for (String f : fs) {
-                    copy(new File(src, f), new File(dest, f));
-                }
-            }
-        }
-        else
-        {
-            try(FileInputStream inStream = new FileInputStream(src);
-                FileOutputStream outStream = new FileOutputStream(dest);)
-            {
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = inStream.read(buffer))>0)
-                {
-                    outStream.write(buffer,0,length);
-                }
-                System.out.println("目录复制成功！");
-                inStream.close();
-                outStream.close();
-            }
-            catch (IOException e)
-            {
-                System.out.println(e);
-            }
-        }
-    }
-
-    void removeDir(File f) {
-        File[]files = f.listFiles();
-        if (files != null)
-        {
-            for (File file : files)
-            {
-                if (file.isDirectory())
-                {
-                    removeDir(f);
-                }
-                else
-                {
-                    file.delete();
-                }
-            }
-        }
-        if (f.exists())
-        {
-            f.delete();
-            System.out.println("成功删除目录!");
-        }
-        else
-        {
-            System.out.println("操作失败，"+f.getName()+"不存在");
-        }
-    }
-
-    void help() {
-        System.out.println("cd ~/Documents -- 转到此目录下 ");
-        System.out.println("ls -- 罗列当前目录下内容");
-        System.out.println("mkdir -- 在当前目录下新建文件夹");
-        System.out.println("rmdir -- 删除当前目录以及目录下所有文件");
-        System.out.println("cp -- 拷贝文件或目录到其他目录下");
-        System.out.println("encrypt -- 加密文件");
-        System.out.println("decrypt -- 解密文件");
-        System.out.println("quit -- 退出程序");
-    }
+  void help() {
+    System.out.println("cd ~/Documents -- 转到此目录下 ");
+    System.out.println("ls -- 罗列当前目录下内容");
+    System.out.println("mkdir -- 在当前目录下新建文件夹");
+    System.out.println("rmdir -- 删除当前目录以及目录下所有文件");
+    System.out.println("cp -- 拷贝文件或目录到其他目录下");
+    System.out.println("encrypt -- 加密文件");
+    System.out.println("decrypt -- 解密文件");
+    System.out.println("quit -- 退出程序");
+  }
 }
